41e62ad627795fa2b4dac5e8a2a9295c6a2f6655
Alice <alice.gugu@gmail.com>
2014-03-10 12:33:07 +0800

    FEATURE: logout

--- urls.py
+++ urls.py
@@ -24,6 +24,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('userprofile.urls')),
+    url(r'^logout/', LogoutView.as_view()),
 )
 
 import os
